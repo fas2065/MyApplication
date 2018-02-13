c98e6fcb8ec6db302dd0ee5fa40fbba22d226e96
fas2065 <farshad.salimi@gmail.com>
2018-02-13 17:02:05 +0100

    Concept proof.

---
+++ app/src/main/java/com/rasco/myapp/myapplication/LightShow.java
@@ -0,0 +1,41 @@
+package com.rasco.myapp.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by Admin on 2/5/2018.
+ */
+
+public class LightShow extends View {
+
+    private float roll = 0;
+
+    public LightShow(Context context){
+        super(context);
+    }
+
+    public void redraw(float roll) {
+        this.roll = roll;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+
+        if (roll < 0) {
+            paint.setColor(Color.RED);
+//            LogUtil.i("Negative roll: " + roll);
+        } else {
+            paint.setColor(Color.BLUE);
+//            LogUtil.i("positive roll: " + roll);
+        }
+        canvas.drawPaint(paint);
+    }
+
+}
